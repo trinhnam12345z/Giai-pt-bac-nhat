997f63c0c47aa59c4742d9306091bfdf6497a182
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-08 17:02:22 +0700

    Giải pt bậc nhất

---
+++ GiaiPTBacNhat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GiaiPTBacNhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CT GIAI PT BAC NHAT");
+            Console.WriteLine("PT Co dang 'a*x + b = 0', Vui long nhap cac tham so :");
+            Console.Write("a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            if (a != 0)
+            {
+                double solution = -b / a;
+                Console.Write("PT co nghiem la : {0}", solution);
+            }
+            else
+            {
+                if (b == 0)
+                {
+                    Console.Write("PT co vo so nghiem !");
+                }
+                else
+                {
+                    Console.Write("PT vo nghiem !");
+                }
+            }
+        }
+    }
+}
